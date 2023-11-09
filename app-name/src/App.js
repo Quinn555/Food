5a19987757f110375b57a7c3c761c9f62f204df0
”Quin555” <QUINTIN.WEST3507@STU.MTEC.EDU>
2023-11-09 10:40:31 -0700

    Main setup practically done Thu Nov  9 10:40:31 MST 2023

---
+++ app-name/src/App.js
@@ -0,0 +1,73 @@
+import { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+  const [mainMenu, setmainMenu] = useState([]);
+  const [dataCat, setDataCat] = useState([]);
+  const [imagesHtml, setImagesHtml] = useState([]);
+
+  //Main screen Selection
+  useEffect(() => {
+    async function getLink() {
+      const url = 'https://www.themealdb.com/api/json/v1/1/categories.php';
+      const response = await fetch(url);
+      const data = await response.json();
+      const dataCat = data.categories;
+      setmainMenu(dataCat);
+      setDataCat(dataCat);
+    }
+
+    getLink();
+  }, []);
+
+  //Selection based off of what user pressed
+  async function getLinkOnButton(imgLink) {
+    const foodSelected = `${imgLink.strCategory ?? imgLink.strMeal}`;
+    let url = '';
+    if (foodSelected === imgLink.strCategory) {
+      url = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${foodSelected}`;
+      console.log('broken');
+    }
+    else{
+      const trueFood = foodSelected.split(' ').join('_');
+      url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${trueFood}`;
+      console.log(url);
+    }
+    const response = await fetch(url);
+    const data = await response.json();
+    const dataCat = data.meals;
+    setDataCat(dataCat);
+  }
+
+  //Displays the stuff
+  useEffect(() => {
+    console.log(dataCat);
+    let imagesArray = dataCat.map((imgLink) => (
+      <div key={imgLink.idCategory ?? imgLink.idMeal}>
+        <button className='buttonStuff' onClick={() => getLinkOnButton(imgLink)}>
+          {imgLink.strCategory ?? imgLink.strMeal}
+        </button>
+        <div className='options'>
+          <img src={imgLink.strCategoryThumb ?? imgLink.strMealThumb} alt={imgLink.strCategory ?? imgLink.strMeal}/>
+        </div>
+        
+      </div>
+    ));
+    setImagesHtml(imagesArray);
+  }, [dataCat]);
+
+
+  return (
+    <div className="App">
+      <div className='main'>
+        <div className='imag-fancy'></div>
+        <div className='stuff-in-menu'>
+          <button className='title' onClick={() => setDataCat(mainMenu)}>Menu</button>
+          <div className='main-menu'>{imagesHtml}</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
